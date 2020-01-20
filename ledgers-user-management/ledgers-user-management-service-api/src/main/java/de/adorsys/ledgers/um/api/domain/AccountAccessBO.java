57049a3e00fd5586955bafccca61676ff8b529cd
olhaz <oza@adorsys.com.ua>
2020-01-20 18:08:40 +0200

    increase code quality. Sonarcloud bugs

--- ledgers-user-management/ledgers-user-management-service-api/src/main/java/de/adorsys/ledgers/um/api/domain/AccountAccessBO.java
+++ ledgers-user-management/ledgers-user-management-service-api/src/main/java/de/adorsys/ledgers/um/api/domain/AccountAccessBO.java
@@ -1,11 +1,15 @@
 package de.adorsys.ledgers.um.api.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Currency;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountAccessBO {
     private String id;
     @NotNull
@@ -22,8 +26,5 @@ public class AccountAccessBO {
     public AccountAccessBO(@NotNull String iban, AccessTypeBO accessType) {
         this.iban = iban;
         this.accessType = accessType;
-    }
-
-    public AccountAccessBO() {
     }
 }
