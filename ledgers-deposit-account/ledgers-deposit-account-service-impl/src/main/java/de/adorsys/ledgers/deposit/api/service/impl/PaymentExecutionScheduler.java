f3a56c74464974435c09afe3fca4b559b7a95f22
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2020-05-06 20:11:13 +0300

    added accounts filtering in payment scheduler.

--- ledgers-deposit-account/ledgers-deposit-account-service-impl/src/main/java/de/adorsys/ledgers/deposit/api/service/impl/PaymentExecutionScheduler.java
+++ ledgers-deposit-account/ledgers-deposit-account-service-impl/src/main/java/de/adorsys/ledgers/deposit/api/service/impl/PaymentExecutionScheduler.java
@@ -1,6 +1,10 @@
 package de.adorsys.ledgers.deposit.api.service.impl;
 
+import de.adorsys.ledgers.deposit.api.domain.TransactionStatusBO;
+import de.adorsys.ledgers.deposit.db.domain.DepositAccount;
 import de.adorsys.ledgers.deposit.db.domain.Payment;
+import de.adorsys.ledgers.deposit.db.domain.PaymentTarget;
+import de.adorsys.ledgers.deposit.db.repository.DepositAccountRepository;
 import de.adorsys.ledgers.deposit.db.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -16,12 +21,43 @@ import java.util.List;
 public class PaymentExecutionScheduler {
     private static final String SCHEDULER = "Scheduler";
     private final PaymentRepository paymentRepository;
+    private final DepositAccountRepository accountRepository;
+
     private final PaymentExecutionService executionService;
 
     @Scheduled(initialDelayString = "${paymentScheduler.initialDelay}", fixedDelayString = "${paymentScheduler.delay}")
     public void scheduler() {
         log.info("Payment Execution Scheduler started at {}", LocalDateTime.now());
         List<Payment> payments = paymentRepository.getAllDuePayments();
-        payments.forEach(p -> executionService.executePayment(p, SCHEDULER));
+        payments.forEach(this::executeIfNotBlocked);
+    }
+
+    private void executeIfNotBlocked(Payment payment) {
+        boolean debtorIsBlocked = isDebtorAccountBlocked(payment.getAccountId());
+        boolean creditorsAreBlocked = areTargetsBlocked(payment.getTargets());
+
+        if (!debtorIsBlocked && !creditorsAreBlocked) {
+            executionService.executePayment(payment, SCHEDULER);
+        }
+    }
+
+    private boolean isDebtorAccountBlocked(String accountId) {
+        return accountRepository.findById(accountId)
+                       .map(this::isDepositAccountBlocked)
+                       .orElse(false);
+    }
+
+    private boolean areTargetsBlocked(List<PaymentTarget> targets) {
+        return targets.stream()
+                       .map(PaymentTarget::getCreditorAccount)
+                       .map(ar -> accountRepository.findByIbanAndCurrency(ar.getIban(), ar.getCurrency())
+                                          .orElse(null))
+                       .anyMatch(this::isDepositAccountBlocked);
+    }
+
+    private boolean isDepositAccountBlocked(DepositAccount depositAccount) {
+        return Optional.ofNullable(depositAccount)
+                       .map(da -> da.isBlocked() || da.isSystemBlocked())
+                       .orElse(false);
     }
 }
