a0796762bc432ab690e1359260f8ef5b2610727d
Dmytro Mishchuk <dmi@adorsys.com.ua>
2020-07-22 12:12:45 +0300

    added gluu configuration class

---
+++ ledgers-middleware/ledgers-middleware-rest-server/src/main/java/de/adorsys/ledgers/middleware/rest/security/ExternalIdpConfiguration.java
@@ -0,0 +1,20 @@
+package de.adorsys.ledgers.middleware.rest.security;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "ledgers.gluu")
+public class ExternalIdpConfiguration {
+    private String clientId;
+    private String clientSecret;
+    private String clientAuthenticationMethod;
+    private String scope;
+    private String authorizationGrantType;
+    private String redirectUri;
+    private String authorizationUri;
+    private String tokenUri;
+    private String userInfoUri;
+}
