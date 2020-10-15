5f07dbb9396ee79460dafd470fd123042aaaab46
Andrii Murashkin <amu@adorsys.com.ua>
2020-10-15 14:29:17 +0300

    #796 reset password via email in keycloak

--- keycloak/keycloak-client/src/main/java/de/adorsys/ledgers/keycloak/client/config/KeycloakClientConfig.java
+++ keycloak/keycloak-client/src/main/java/de/adorsys/ledgers/keycloak/client/config/KeycloakClientConfig.java
@@ -1,8 +1,13 @@
 package de.adorsys.ledgers.keycloak.client.config;
 
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @Configuration
@@ -34,7 +39,42 @@ public class KeycloakClientConfig {
     @Value("${keycloak.public-client}")
     private boolean publicClient;
 
-    public String getRootPath() {
-        return authServerUrl + "/realms/" + clientRealm;
+    @Value("${keycloak.email.server.host:}")
+    private String emailHost;
+
+    @Value("${keycloak.email.server.port:}")
+    private String emailPort;
+
+    @Value("${keycloak.email.server.ssl:false}")
+    private String emailSsl;
+
+    @Value("${keycloak.email.server.auth:false}")
+    private String emailAuth;
+
+    @Value("${keycloak.email.server.user:}")
+    private String emailUser;
+
+    @Value("${keycloak.email.server.password:}")
+    private String emailPassword;
+
+    @Value("${keycloak.email.server.from:}")
+    private String emailFrom;
+
+    @Value("${keycloak.email.server.fromDisplayName:}")
+    private String emailFromDisplayName;
+
+    @Getter
+    private Map<String, String> smtpServer = new HashMap<>();
+
+    @PostConstruct
+    public void doInit() {
+        smtpServer.put("host", emailHost);
+        smtpServer.put("port", emailPort);
+        smtpServer.put("auth", emailAuth);
+        smtpServer.put("ssl", emailSsl);
+        smtpServer.put("from", emailFrom);
+        smtpServer.put("fromDisplayName", emailFromDisplayName);
+        smtpServer.put("user", emailUser);
+        smtpServer.put("password", emailPassword);
     }
 }
